b127fedeb2e86a35f4b0e9a9c6a53cfb1143d180
facquer <angel.ichazu@gmail.com>
2020-07-26 21:27:24 -0500

    Mister conflictos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -15,31 +15,14 @@ const routes: Routes = [
   {
     path: 'listar-rol',
     loadChildren: () => import('./shared/paginas/listar-rol/listar-rol.module').then( m => m.ListarRolPageModule)
-  },  {
-    path: 'crear-rol',
-    loadChildren: () => import('./shared/paginas/crear-rol/crear-rol.module').then( m => m.CrearRolPageModule)
-=======
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'crear-rol',
+    loadChildren: () => import('./shared/paginas/crear-rol/crear-rol.module').then( m => m.CrearRolPageModule)
   },
   {
     path: 'crear-paciente',
     loadChildren: () => import('./crear-paciente/crear-paciente.module').then( m => m.CrearPacientePageModule)
-  },
-  {
-    path: 'listar-rol',
-    loadChildren: () => import('./shared/paginas/listar-rol/listar-rol.module').then( m => m.ListarRolPageModule)
->>>>>>> 9fbc306b4b3e29c3669921bb4d84392c320d575c
   }
 
 ];
